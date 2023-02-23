969dcb35903d1b9eab02a727977d8a9f2f639c59
emre cakir <ynsckr01@gmail.com>
2023-02-23 14:23:57 +0300

    tour places

---
+++ src/App.js
@@ -0,0 +1,22 @@
+
+import Header from "./components/header/Header";
+import Navbar from "./components/navbar/Navbar";
+import Card from "./components/card/Card";
+
+import {data} from './helper/data';
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <Header/>
+      <div className="container">
+        {data.map(({id, title,desc,image})=>(
+        <Card id={id} title={title} desc={desc} image={image}/>
+      ))}
+      </div>
+      
+    </div>
+  );
+}
+
+export default App;
